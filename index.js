3724f3b2b7cd2ec7f078bda4dbdf1f2255b29974
Trigggerwarning <pasin.kon@student.mahidol.edu>
2026-03-16 15:40:11 +0700

    Fix typo in response message in requestListener

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const requestListener = (req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Worl from DevOps Lab!\n');
+    res.end('Hello World from DevOps Lab!\n');
 };
 
 const app = http.createServer(requestListener);
